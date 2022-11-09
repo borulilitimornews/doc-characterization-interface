2e90e6cb9052e3d9ed146deabcdb02549b88ab64
Gabriel <timornewstl@gmail.com>
2022-11-09 20:24:57 +0000

    minor improvement on template

--- doccharacterizationproject/doccharacterizationapp/urls.py
+++ doccharacterizationproject/doccharacterizationapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('news/list/', views.index, name='news-list'),
     #path('news/add/', views.news_add, name='news-add'),
     path('news/<int:news_id>/detail/', views.news_detail, name='news-detail'),
-    path('news/<int:news_id>/edit/', views.news_edit, name='news-edit'),
+    path('news/<int:news_id>/change/', views.news_edit, name='news-edit'),
     #path('news/<int:news_id>/confirm/delete/', views.news_confirm_delete, name='news-confirm-delete'),
     #path('news/<int:news_id>/delete/', views.news_delete, name='news-delete'),
 ]
